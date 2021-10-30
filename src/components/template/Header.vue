41fd261d11b1f662369b5a88bc97e5d4535d4916
miithrandiir <simon.heban@ferenost.fr>
2021-10-30 14:43:27 +0200

    feat: header

---
+++ src/components/template/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div class="top">
+      <h1>Simon HEBAN</h1>
+      <nav>
+        <ul>
+          <li><a href="">Accueil</a></li>
+          <li><a href="">Compétences</a></li>
+          <li><a href="">Réalisations</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="bottom">
+      <div class="img">
+        <img src="../../assets/img/undraw_outer_space_re_u9vd.svg" alt=""/>
+      </div>
+      <div class="title">
+        <h1>Développeur Informatique</h1>
+        <h2>Passionné - curieux</h2>
+      </div>
+    </div>
+  </header>
+  <svg viewBox="0 0 1445 339" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1445 0C1366.16 36.0723 1258.74 45.7629 1139.38 56.5309C971.943 71.6353 781.011 88.8594 612.5 183.991C260 382.991 0 332.991 0 332.991V0H1445Z" fill="#363795"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
