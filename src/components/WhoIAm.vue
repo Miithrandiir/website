a6f8b4d6d4258df2b651bb30fd4b8e52c879a2f9
miithrandiir <simon.heban@ferenost.fr>
2021-10-30 17:11:15 +0200

    feat: "WhoIAm" component added

---
+++ src/components/WhoIAm.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Qui suis-je ?</h1>
+
+  <div class="cote">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec nulla in sem consectetur pharetra. Vivamus molestie odio eu metus blandit dictum. Ut sit amet elementum lectus, a placerat justo. Duis sit amet commodo risus. Fusce id pellentesque erat. Maecenas ut sagittis risus. Vestibulum in tellus libero. Aliquam lacinia urna non elementum tempus. Suspendisse commodo, erat et placerat efficitur, dui erat dignissim quam, aliquam mollis tortor erat at erat.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WhoIAm"
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/sass/_fonts.scss";
+@import "../assets/sass/_vars.scss";
+
+  h1 {
+    font-family: "Raleway", sans-serif;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  .cote {
+    display: flex;
+    margin: 100px auto;
+    width: 1000px;
+    border-left: 10px solid $main;
+    &>p {
+      margin: 0 10px;
+      font-size: 18px;
+      font-family: "Raleway", sans-serif;
+
+    }
+  }
+</style>
