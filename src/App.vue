e64dd46f0d31511dd3d385c24b6b256e412c54d1
miithrandiir <simon.heban@ferenost.fr>
2021-10-31 11:39:16 +0100

    feat: add "Footer" & "Quote" components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 
 import Header from "./components/template/Header.vue";
+import Footer from "./components/template/Footer.vue";
 import './assets/sass/template.scss'
 import WhoIAm from "./components/WhoIAm.vue";
 import Passions from "./components/Passions.vue";
 import Creations from "./components/Creations.vue";
+import Quote from "./components/Quote.vue";
 </script>
 
 <template>
@@ -13,4 +15,6 @@ import Creations from "./components/Creations.vue";
   <WhoIAm/>
   <Passions/>
   <Creations/>
+  <Quote/>
+  <Footer/>
 </template>
