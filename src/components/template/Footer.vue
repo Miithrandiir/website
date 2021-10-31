08952d83130012f2ab266e427a59cf4c24da8434
miithrandiir <simon.heban@ferenost.fr>
2021-10-31 11:38:59 +0100

    feat: add "Footer" components

---
+++ src/components/template/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="categories">
+      <div class="category">
+        <div class="title">
+          SIMON HEBAN
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a erat ac quam condimentum bibendum et ut sapien. Praesent quis nibh maximus, vehicula turpis quis, sodales odio.
+        </p>
+      </div>
+      <div class="category">
+        <div class="title">
+          Liens
+        </div>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Mes compétences</a></li>
+          <li><a href="#">Mes réalisations</a></li>
+          <li><a href="#">Mentions légales</a></li>
+        </ul>
+
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
