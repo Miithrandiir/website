54274e514512dc145ae3f439b3bf75c4e18a73ad
miithrandiir <simon.heban@ferenost.fr>
2021-11-01 18:34:52 +0100

    feat: add responsive code to header

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -4,11 +4,12 @@
       <h1>Simon HEBAN</h1>
       <nav>
         <ul>
-          <li><a href="#">Accueil</a></li>
-          <li><a href="#">Compétences</a></li>
+          <li><router-link to="/">Accueil</router-link></li>
+          <li><router-link to="/skills">Compétences</router-link></li>
           <li><a href="#">Réalisations</a></li>
         </ul>
       </nav>
+      <a href="#" class="hamburger-nav" @click.prevent="show()"><i class="fas fa-bars"></i></a>
     </div>
     <div class="bottom">
       <div class="img">
@@ -21,13 +22,34 @@
     </div>
   </header>
   <svg viewBox="0 0 1445 339" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1445 0C1366.16 36.0723 1258.74 45.7629 1139.38 56.5309C971.943 71.6353 781.011 88.8594 612.5 183.991C260 382.991 0 332.991 0 332.991V0H1445Z" fill="#363795"/>
+    <path
+        d="M1445 0C1366.16 36.0723 1258.74 45.7629 1139.38 56.5309C971.943 71.6353 781.011 88.8594 612.5 183.991C260 382.991 0 332.991 0 332.991V0H1445Z"
+        fill="#363795"/>
   </svg>
 </template>
 
-<script>
+<script lang="ts">
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 export default {
-  name: "Header"
+  name: "Header",
+  data() {
+    return {
+      "nav_show": true
+    }
+  },
+  methods: {
+    show() {
+      let item: HTMLElement | null = document.querySelector("nav");
+      if (item != null) {
+        if (item.classList.contains("show")) {
+          item.classList.remove("show");
+        } else {
+          item.classList.add("show")
+        }
+      }
+
+    }
+  }
 }
 </script>
-
