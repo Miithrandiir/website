3d142c46d030bd590ab5cfc3b3a8754bbfe4b955
miithrandiir <simon.heban@ferenost.fr>
2021-11-01 18:34:21 +0100

    refactor: split in pages

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,10 @@
 import Header from "./components/template/Header.vue";
 import Footer from "./components/template/Footer.vue";
 import './assets/sass/template.scss'
-import WhoIAm from "./components/WhoIAm.vue";
-import Passions from "./components/Passions.vue";
-import Creations from "./components/Creations.vue";
-import Quote from "./components/Quote.vue";
 </script>
 
 <template>
   <Header/>
-  <WhoIAm/>
-  <Passions/>
-  <Creations/>
-  <Quote/>
+  <router-view/>
   <Footer/>
 </template>
